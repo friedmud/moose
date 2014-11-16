d64d27432b1f88c924b137ccce733dcb32475be2
Derek Gaston <derek.gaston@inl.gov>
2014-11-16 11:50:29 -0500

    disable mat_new_nonzero_allocation_err suppression refs #4230

--- framework/src/base/NonlinearSystem.C
+++ framework/src/base/NonlinearSystem.C
@@ -1652,7 +1652,7 @@ NonlinearSystem::computeJacobianInternal(SparseMatrix<Number> &  jacobian)
 #if PETSC_VERSION_LESS_THAN(3,3,0)
 #else
   // PETSc 3.3.0
-  MatSetOption(static_cast<PetscMatrix<Number> &>(*sys().matrix).mat(), MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
+//  MatSetOption(static_cast<PetscMatrix<Number> &>(*sys().matrix).mat(), MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
 #endif
 
 #endif
@@ -1826,7 +1826,7 @@ NonlinearSystem::computeJacobianBlocks(std::vector<JacobianBlock *> & blocks)
 #if PETSC_VERSION_LESS_THAN(3,3,0)
 #else
     // PETSc 3.3.0
-    MatSetOption(static_cast<PetscMatrix<Number> &>(jacobian).mat(), MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
+//    MatSetOption(static_cast<PetscMatrix<Number> &>(jacobian).mat(), MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
 #endif
 
 #endif
